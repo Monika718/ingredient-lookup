c99cf078b3727c7368de01efb4aaab87e056f56f
Monika Zhong <zhong.xiabei@gmail.com>
2020-07-23 14:26:06 -0400

    updated blacklist-lookup to check if substring and added more ingredients in bl

--- src/ingredient_lookup/process_item.clj
+++ src/ingredient_lookup/process_item.clj
@@ -5,8 +5,8 @@
 (defn trim-str
   "Keep letters, digits, slash and comma of a given string"
   [s]
-  (let [no-space-s (str/replace s #", | ," ",")
-        cleaned-up-s (apply str (re-seq #"[a-zA-Z0-9\-\s,\/]" no-space-s))]
+  (let [no-space-s (str/replace s #", | ,|･" ",")
+        cleaned-up-s (apply str (re-seq #"[a-zA-Z\-\s,\/]" no-space-s))]
     cleaned-up-s))
 
 (defn blacklist-lookup
@@ -16,6 +16,10 @@
   (let [items (str/split (trim-str st) #",|\/")
         lower-case-items (map str/lower-case items)
         trimmed-item (map str/trim lower-case-items)]
-    (if-not (empty? (set/intersection bl (set trimmed-item)))
+    (doseq [s trimmed-item]
+      (doseq [b ingredient-lookup.blacklist/makeup-blacklist]
+        (if (str/includes? s b)
+          (println "Found" b "in the ingredient"))))
+    #_(if-not (empty? (set/intersection bl (set trimmed-item)))
       (println "Found" (set/intersection bl (set trimmed-item)) "in the ingredient. This product is not suitable for pregnant women.")
       (println "Product is safe for pregnant women to use."))))
